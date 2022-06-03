10dcf3e23415a28b615eae08c7166290a4e546ba
natnael endale <62070546+natani51@users.noreply.github.com>
2022-06-03 09:42:58 -0800

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-         char c;
-          for (c = 'a'; c <= 'z'; c++)
-          if (c != 'e' && c != 'q')
-         putchar(c);
-       putchar('\n');
-      return (0);
+ char c;
+ for (c = 'a'; c <= 'z'; c++)
+ if (c != 'e' && c != 'q')
+ putchar(c);
+ putchar('\n');
+ return (0);
   }
