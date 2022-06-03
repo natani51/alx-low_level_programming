908b97a27c719afccebe9d4408c60871e6533913
natani51 <natnaelendale51@gmail.com>
2022-06-03 13:09:00 -0700

    print_tebahpla.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,14 +8,11 @@
  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+int i;
+for (i = 0; i < 10; i++)
 {
-if (ch != 'e' && ch != 'q')
-{
-	putchar(ch);
+	printf("%i", i);
 }
-}
-putchar('\n');
+printf("\n");
 return (0);
 }
