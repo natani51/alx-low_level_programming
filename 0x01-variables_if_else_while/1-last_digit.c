48e192947ef333ccaded4173f6571c9c095040db
natnael endale <62070546+natani51@users.noreply.github.com>
2022-06-03 09:23:09 -0800

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - finds the last int in a random number
- *
- * Description: The numbers are generated automatically
- * Return: Always(0) Success
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Brennan D Baraban
  */
 
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int n, last_digit;
+	char letter;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
-	printf("Last digit of %d is %d and is ", n, last_digit);
-	if (last_digit > 5)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		printf("grater than 5\n");
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
-	else if (last_digit  == 0)
-	{
-		printf("0\n");
-	}
-	else if (last_digit < 6 && last_digit != 0)
-	{
-		printf("less than 6 and not 0\n");
-	}
+
+	putchar('\n');
+
 	return (0);
 }
