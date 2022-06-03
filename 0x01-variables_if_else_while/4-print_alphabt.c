acfacf96fc002fc288cd9df03716da38c100c737
natani51 <natnaelendale51@gmail.com>
2022-06-03 13:04:58 -0700

    print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 /**
- *  main - return a-z except q and e
- *  Return: Always 0.
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
-char c;
-
-for (c = 'a'; c <= 'z'; c++)
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	if (c != 'q' && c != 'e')
-	putchar(c);
-
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
 }
 putchar('\n');
-
-return (0)
+return (0);
 }
