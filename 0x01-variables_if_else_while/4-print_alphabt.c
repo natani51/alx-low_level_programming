c23f4b89c00bb9490ed9ff1b4a66d271e0073403
natani51 <natnaelendale51@gmail.com>
2022-06-03 12:19:26 -0700

    print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,19 +3,18 @@
 /**
  * main - prints out the alphabet in lowercase except for 'qe'
  *
- * Return (0)
+ * Return: Always 0
  */
-.B betty-doc
 int main(void)
 {
-          char c;
+           char c;
+        {
           for (c = 'a'; c <= 'z'; c++)
           if (c != 'e' && c != 'q')
-   
-        putchar(c);
+              putchar(c);
   
+	}
  putchar('\n');
   
  return (0);
-  
 }
