3548421cd070cf457f1b56a0e88481969178d5c1
natnael endale <62070546+natani51@users.noreply.github.com>
2022-06-08 13:33:59 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ *0-putchar.c
+ */
+#include "main.h"
+/**
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+ -putchar('_');
+ -putchar('p');
+ -putchar('u');
+ -putchar('t');
+ -putchar('c');
+ -putchar('h');
+ -putchar('a');
+ -putchar('r');
+ -putchar('/n');
+ return (0);
+}
