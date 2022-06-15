92fdb5c5236c6314237af6cd7a7c837a1a22c866
natnael endale <62070546+natani51@users.noreply.github.com>
2022-06-15 10:03:14 -0800

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,66 +1,26 @@
 #include "main.h"
 
 /**
-  * _strncpy - Copy a string
-  * @dest: The destination value
-  * @src: The source value
-  * @n: The copy limit
-  *
-  * Return: char value
-  */
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0, b = 0;
+	int index = 0, src_len = 0;
 
-	while (src[b])
-	{
-		b++;
-	}
+	while (src[index++])
+		src_len++;
 
-	while (a < n && src[a])
-	{
-		dest[a] = src[a];
-		a++;
-	}
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
 
-	while (a < n)
-	{
-		dest[a] = '\0';
-		a++;
-	}
-
-	return (dest);
-}
-#include "holberton.h"
-
-/**
-  * _strncpy - Copy a string
-  * @dest: The destination value
-  * @src: The source value
-  * @n: The copy limit
-  *
-  * Return: char value
-  */
-char *_strncpy(char *dest, char *src, int n)
-{
-	int a = 0, b = 0;
-
-	while (src[b])
-	{
-		b++;
-	}
-
-	while (a < n && src[a])
-	{
-		dest[a] = src[a];
-		a++;
-	}
-
-	while (a < n)
-	{
-		dest[a] = '\0';
-		a++;
-	}
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
 	return (dest);
 }
