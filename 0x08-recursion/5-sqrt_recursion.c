65e936ea9b3b16e27a0facb3ac23d34d8b9f80ce
natani51 <natnaelendale51@gmail.com>
2022-06-22 00:40:00 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,29 @@
 #include "main.h"
 
 /**
- * prime2 - Makes possible to evaluate from 1 to n
+ * sqrt2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int prime2(int a, int b)
+int sqrt2(int a, int b)
 {
-	if (a == b)
-		return (1);
-	else if (a % b == 0)
-		return (0);
-	return (prime2(a, b + 1));
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
 }
 /**
- * is_prime_number - checks if a number is prime
+ * _sqrt_recursion - returns the natural square root of n
  * @n: Number Integer
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int is_prime_number(int n)
+int _sqrt_recursion(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (prime2(n, 2));
+	return (sqrt2(n, 1));
 }
