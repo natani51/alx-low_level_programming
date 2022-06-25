ec30668a7753f8928cb9050a34c858fb3f10061d
natnael endale <62070546+natani51@users.noreply.github.com>
2022-06-25 13:18:02 -0800

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,24 +1,19 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * create_array - create array
- * @size: size
- * @c: constant
- * Return: char pointer
- */
-char *create_array(unsigned int size, char c)
-{
-	char *p;
-	unsigned int i;
+#include <limits.h>
 
-	if (size == 0)
-	return (0);
-	p = malloc((size) * sizeof(char));
-	if (p == '\0')
-	return (0);
-	for (i = 0; i < size; i++)
-	{
-		p[i] = c;
-	}
-return (p);
+/**
+ * *malloc_checked - allocate memory with malloc
+ * @b: unsigned int type
+ * Return: return pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
