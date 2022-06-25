db5adc247bd9b57e9227ab684bcd592338a3ab61
natnael endale <62070546+natani51@users.noreply.github.com>
2022-06-25 13:23:08 -0800

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,68 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * alloc_grid - prints a grid of integers
- * @width: width of the grid
- * @height: height of the grid
+ * _strlen - return length of a string
  *
- * Return: pointer..
+ * @s: char type
+ * Return:  length of string
  */
-int **alloc_grid(int width, int height)
+int _strlen(char *s)
 {
-int **s, r, c;
+	int a;
 
-	if (width <= 0 || height <= 0)
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	return (a);
+}
+/**
+ * _strncat - function to concatnate strings with n bytes
+ *
+ * @dest: destination for concatnation
+ * @src: source of string
+ * @n: int type for size of byte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, a;
+
+	dest_len = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+	return (dest);
+}
+
+/**
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int size1, size2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+
+	ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-
-	s = malloc(sizeof(int *) * height);
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-
-	for (r = 0; r < height; r++)
-	{
-		s[r] = malloc(sizeof(int) * width);
-
-		if (s[r] == NULL)
-		{
-			for (; r >= 0; r--)
-			{
-				free(s[r]);
-			}
-			free(s);
-			return (NULL);
-		}
-
-		for (c = 0; c <= width; c++)
-		{
-			s[r][c] = 0;
-		}
-	}
-	return (s);
-
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	ptr += '\0';
+	return (ptr);
 }
