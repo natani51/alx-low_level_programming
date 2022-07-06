11947b6b2e25018c09355f11e832db71af38c74e
natani51 <natnaelendale51@gmail.com>
2022-07-05 22:55:28 -0700

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FILE_F_POINTRS
+#define FILE_F_POINTRS
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
